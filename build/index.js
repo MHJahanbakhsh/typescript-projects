f16f5d28c1c0c052c40ca056b6c53b9045c1335c
mohammadhossein <mammadjahanbakhsh@yahoo.com>
2022-09-27 23:04:00 +0330

    showing bad approach to sort diffrent types.untill video 80

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 "use strict";
 class Sorter {
-    constructor(collection) {
+    constructor(collection /*| LinkedList*/) {
         this.collection = collection;
     }
     //bubble sort algorithm
@@ -8,12 +8,25 @@ class Sorter {
         const { length } = this.collection;
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    console.log({ j });
-                    const leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
-                    console.log(this.collection);
+                /*the reason that we call this bad approach is that everytime
+                we want to add another type  to be sorted, first we have to add that
+                to cunstructor function and then put another if statement here:
+                
+                if(this.collection instanceof LinkedList){....}
+                 */
+                //only works,if collection is an array of numbers
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        console.log({ j });
+                        const leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                        console.log(this.collection);
+                    }
+                }
+                //if collection is string;do this logic instead
+                //~~~logic to compare and swap characters in a string
+                if (typeof this.collection === 'string') {
                 }
             }
         }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 class Sorter{
-    constructor(public collection:number[]){}
+    constructor(public collection:number[] | string /*| LinkedList*/ ){}
 
     //bubble sort algorithm
     sort():void{
@@ -7,13 +7,30 @@ class Sorter{
 
         for(let i=0; i<length; i++){
             for(let j=0; j<length-i-1;j++){
+
+                /*the reason that we call this bad approach is that everytime 
+                we want to add another type  to be sorted, first we have to add that 
+                to cunstructor function and then put another if statement here:
+                
+                if(this.collection instanceof LinkedList){....}
+                 */
+
+                //only works,if collection is an array of numbers
+                if(this.collection instanceof Array){
             if(this.collection[j]>this.collection[j+1]){
                 console.log({j})
                 const leftHand = this.collection[j]
                 this.collection[j] = this.collection[j+1]
                 this.collection[j+1] = leftHand
                 console.log(this.collection)
+            }}
+
+            //if collection is string;do this logic instead
+            //~~~logic to compare and swap characters in a string
+            if(typeof this.collection==='string'){
+                
             }
+
         }
     }
     }
